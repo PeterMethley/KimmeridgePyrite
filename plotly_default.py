79ade0d4d52bb07f0cd6586db7ae2c3346759506
Peter Methley <peter.methley@gmail.com>
2022-06-02 13:23:37 +0100

    Plots optimised for posters/presentations

--- plotly_default.py
+++ plotly_default.py
@@ -34,24 +34,29 @@ def sel_trace(plot, value, prop='name'):
 
 
 # Visual style of Plotly graphs
-axis_layout = {'showline': True, 'mirror': True, 'ticks': 'outside', 'showgrid': True, 'zeroline': True, 'linewidth': 1.3, 'color': 'black'}
 
-axis_layout_3D = dict(showline = True, ticks = 'outside', linewidth = 2, color = 'black')
+black_color = '#231F20' # CMYK 100% black
+grid_color = '#E2E7ED' # Default is #eee
+
+axis_layout = {'showline': True, 'mirror': True, 'ticks': 'outside', 'showgrid': True, 'zeroline': True, 'linewidth': 1.3, 'color': black_color, 'gridcolor': grid_color}
+axis_layout_3D = dict(showline = True, ticks = 'outside', linewidth = 2, color = black_color, gridcolor=grid_color)
 
 pio.templates['PM_graph_1'] = go.layout.Template(layout = dict(xaxis = axis_layout,
                           yaxis = axis_layout,
                           bargap = 0.05, bargroupgap = 0,
                           height = 700,
-                          margin = dict(l=90, r=30, t=30, b=60),
+                          margin = dict(l=100, r=30, t=30, b=70),
                           hovermode = 'closest',
                           scene = {'xaxis': axis_layout_3D, 'yaxis': axis_layout_3D, 'zaxis': axis_layout_3D},
-                          font_family = 'Segoe UI', font_size=17, font_color='black'))
+                          font_family = 'Segoe UI', font_size=20, font_color=black_color, # Default font size = 1
+                          plot_bgcolor = 'rgba(255,255,255,180)',
+                          paper_bgcolor = 'rgba(255,255,255,0)'))
                          
 pio.templates.default='PM_graph_1'
 
 graph_config = {
   'toImageButtonOptions': {
-    'format': 'svg', # one of png, svg, jpeg, webp
+    'format': 'pdf', # one of png, svg, jpeg, webp
     'filename': 'plot_capture',
     'scale': 1 # Multiply title/legend/axis/canvas sizes by this factor
   },
